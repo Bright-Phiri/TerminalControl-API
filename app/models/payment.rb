2a3b30539c90e02682f67f4833a52854f102e704
Bright Issah <bphiri.aki@gmail.com>
2025-02-02 20:04:01 +0200

    Add models and migrations wip

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Payment < ApplicationRecord
-  VALID_PAYMENT_METHODS = [ "CASH", "TNM MPAMBA", "AIRTEL MONEY", "VISA" ]
+  VALID_PAYMENT_METHODS = [ "CASH", "TNM MPAMBA", "AIRTEL MONEY", "VISA" ].freeze
   belongs_to :taxpayer
   validates :period, :payment_method, :amount, presence: true
   validates :payment_method, inclusion: { in: VALID_PAYMENT_METHODS }
