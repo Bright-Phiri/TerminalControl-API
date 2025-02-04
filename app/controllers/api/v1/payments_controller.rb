bb5cdba07424829e00bdc4030043147945c66459
Bright Issah <bphiri.aki@gmail.com>
2025-02-04 09:52:54 +0200

    WIP

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -1,18 +1,45 @@
 # frozen_string_literal: true
 
 class API::V1::PaymentsController < ApplicationController
+  before_action :set_payment, only: %i[show update destroy]
   def index
+    payments = Payment.all
+    render json: payments
   end
 
   def create
+    payment = Payment.new(payment_params)
+    if payment.save
+      render json: payment, status: :created
+    else
+      render json: payment.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show
+    render json @payment
   end
 
   def update
+    if @payment.update(payment_params)
+      render json: @payment
+    else
+      render json: @payment.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @payment.destroy!
+    head :no_content
+  end
+
+  private
+
+  def set_payment
+    @payment = Payment.find(params[:id])
+  end
+
+  def payment_params
+    params.expect(payment: [ :period, :amount, :payment_method ])
   end
 end
