601f311ef4ff1afc7e5d86dd5fecd9b039a0b280
Bright Issah <bphiri.aki@gmail.com>
2025-02-04 08:58:05 +0200

    feat: Add api authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,54 @@
 
 class ApplicationController < ActionController::API
   include ExceptionHandler
+
+  private
+
+  def encode_token(payload)
+    JWT.encode(payload, hmac_secret)
+  end
+
+  def logged_in?
+    logged_in_user.present?
+  end
+
+  def logged_in_user
+    return unless decoded_token
+
+    user_id = decoded_token[0]["user_id"]
+    User.find_by(id: user_id)
+  end
+
+  def authorize_request
+    if auth_header.blank?
+      render_unauthorized "Token missing"
+    else
+      render_unauthorized "Invalid token format" and return unless auth_header.starts_with?("Bearer ")
+
+      render_unauthorized "Unauthorized: Invalid or expired token" unless logged_in?
+    end
+  end
+
+  def auth_header
+    request.authorization
+  end
+
+  def decode_token(token)
+    JWT.decode(token, hmac_secret, true, { algorithm: "HS256" })
+  rescue JWT::DecodeError
+    nil
+  end
+
+  def decoded_token
+    token = auth_header.split(" ")[1]
+    decode_token(token)
+  end
+
+  def render_unauthorized(message)
+    render json: { status: "login", message: }, status: :unauthorized
+  end
+
+  def hmac_secret
+    Rails.application.credentials.secret_key_base
+  end
 end
