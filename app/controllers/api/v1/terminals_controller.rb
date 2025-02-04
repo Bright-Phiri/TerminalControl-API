7e3700dc0ba87521e26f3506e7e545b098969cd3
Bright Issah <bphiri.aki@gmail.com>
2025-02-04 09:40:50 +0200

    WIP

--- app/controllers/api/v1/terminals_controller.rb
+++ app/controllers/api/v1/terminals_controller.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 class API::V1::TerminalsController < ApplicationController
+  before_action :set_terminal, only: %i[show]
   def index
     terminals = Terminal.all
     render json: terminals
+  end
+
+  def show
+    render json: @terminal
+  end
+
+  private
+
+  def set_terminal
+    @terminal = Terminal.find(params[:id])
   end
 end
