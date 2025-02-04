f9835e42e238e9a12394163cff7aca140d982928
Bright Issah <bphiri.aki@gmail.com>
2025-02-04 14:49:03 +0200

    WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   VALID_ROLES = [ "Officer", "Admin" ].freeze!
 
+  has_secure_password
+
   validates :user_name, presence: true, format: { without: /\s/, message: "must contain no spaces" }
   validates :email_address, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "is invalid" }
   validates :role, inclusion: { in: VALID_ROLES }
