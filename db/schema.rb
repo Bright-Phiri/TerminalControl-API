2e3e4fbfdcca05ce1926c130ef030ff832800adb
Bright Issah <bphiri.aki@gmail.com>
2025-02-04 08:31:29 +0200

    Add models and migrations wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,40 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_02_174836) do
+ActiveRecord::Schema[8.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
-
-  create_table "payments", force: :cascade do |t|
-    t.bigint "taxpayer_id", null: false
-    t.date "period"
-    t.decimal "amount"
-    t.string "payment_method", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["taxpayer_id"], name: "index_payments_on_taxpayer_id"
-  end
-
-  create_table "taxpayers", force: :cascade do |t|
-    t.string "tin", null: false
-    t.string "name", null: false
-    t.string "email_address", null: false
-    t.string "phone_number", null: false
-    t.integer "terminals_count"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "terminals", force: :cascade do |t|
-    t.string "terminal_id", null: false
-    t.string "terminal_label", null: false
-    t.string "activation_date", null: false
-    t.integer "status"
-    t.bigint "taxpayer_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["taxpayer_id"], name: "index_terminals_on_taxpayer_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -55,7 +24,4 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_02_174836) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  add_foreign_key "payments", "taxpayers"
-  add_foreign_key "terminals", "taxpayers"
 end
