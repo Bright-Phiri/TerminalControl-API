378796c122885dbc56e196b0b03783035a25adc4
Bright Issah <bphiri.aki@gmail.com>
2025-02-04 09:24:03 +0200

    WIP

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,46 @@
 # frozen_string_literal: true
 
 class API::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destroy]
+
   def index
+    users = User.all
+    render json: users
+  end
+
+  def show
+    render json @user
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy!
+    head :no_content
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.expect(user: [ :first_name, :last_name, :user_name, :role, :email_address, :phone_number, :password, :password_confirmation ])
   end
 end
