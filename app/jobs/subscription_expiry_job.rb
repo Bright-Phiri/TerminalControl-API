5630c1ef7b712777313c1b45eec70f4aefefb02b
Bright Issah <bphiri.aki@gmail.com>
2025-02-10 14:28:33 +0200

    WIP

---
+++ app/jobs/subscription_expiry_job.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class SubscriptionExpiryJob < ApplicationJob
+  queue_as :default
+
+    def perform
+      # Find subscriptions that will expire in the next 14 days
+      subscriptions = Subscription.where("end_date BETWEEN ? AND ?", Time.current, Time.current + 14.days)
+      subscriptions.find_each do |subscription|
+        SubscriptionMailer.with(subscription: subscription).subscription_about_to_expire(subscription).deliver_later
+      end
+    end
+end
---
+++ test/jobs/subscription_expiry_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SubscriptionExpiryJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
