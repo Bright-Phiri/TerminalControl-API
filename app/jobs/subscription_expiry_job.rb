4cfe6c28e522784468303c2234ff4825aed8dda2
Bright Issah <bphiri.aki@gmail.com>
2025-02-11 13:36:51 +0200

    WIP

--- app/jobs/subscription_expiry_job.rb
+++ app/jobs/subscription_expiry_job.rb
@@ -2,10 +2,9 @@
 
 class SubscriptionExpiryJob < ApplicationJob
   queue_as :default
-
     def perform
-      # Find subscriptions that will expire in the next 14 days
-      subscriptions = Subscription.where("end_date BETWEEN ? AND ?", Time.current, Time.current + 14.days)
+      # Find subscriptions that will expire in the next 7 days
+      subscriptions = Subscription.where("end_date BETWEEN ? AND ?", Time.current, Time.current + 7.days)
       subscriptions.find_each do |subscription|
         SubscriptionMailer.with(subscription: subscription).subscription_about_to_expire.deliver_later
       end
