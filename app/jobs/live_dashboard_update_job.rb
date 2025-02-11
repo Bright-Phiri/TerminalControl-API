75866c5616b964041d1add17989dd14e02a37f4c
Bright Issah <bphiri.aki@gmail.com>
2025-02-11 14:13:25 +0200

    Add live updates job

--- app/jobs/live_dashboard_update_job.rb
+++ app/jobs/live_dashboard_update_job.rb
@@ -3,7 +3,15 @@
 class LiveDashboardUpdateJob < ApplicationJob
   queue_as :dashboard_updates
 
-  def perform(*args)
-    # Do something later
+  def perform
+    data = {
+      total_clients: Taxpayer.count,
+      total_subscriptions: Subscription.count,
+      active_subscriptions: Subscription.active.count,
+      total_terminals: Terminal.count,
+      total_payments: Payment.sum(:amount)
+    }
+
+    ActionCable.server.broadcast("dashboard_channel", data)
   end
 end
