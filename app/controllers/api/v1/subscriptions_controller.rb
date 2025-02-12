0e36758b6710d735f4afce52462e1b4ea2e695cc
Bright Issah <bphiri.aki@gmail.com>
2025-02-12 12:45:30 +0200

    WIP

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::SubscriptionsController < ApplicationController
-  before_action :set_subscription, only: %i[show renew_subscription destroy]
+  before_action :set_subscription, only: %i[show renew_subscription show_payments destroy]
 
   def index
     subscriptions = Subscription.paginate(page: params[:page] || 1, per_page: params[:per_page] || 10)
@@ -54,9 +54,7 @@ class Api::V1::SubscriptionsController < ApplicationController
   end
 
   def show_payments
-    subscription = Subscription.find(params[:id])
-    payments = subscription.payments.order(:created_at).reverse_order.paginate(page: params[:page] || 1, per_page: params[:per_page] || 10)
-
+    payments = @subscription.payments.order(:created_at).reverse_order.paginate(page: params[:page] || 1, per_page: params[:per_page] || 10)
     render_ok ({ payments: PaymentsRepresenter.new(payments).as_json, total: payments.total_entries })
   end
 
