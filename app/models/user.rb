487d85441d27b64e50c62cfd82e26fdc66b2a047
Bright Issah <bphiri.aki@gmail.com>
2025-02-13 09:34:24 +0200

    Update model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,12 @@ class User < ApplicationRecord
   VALID_ROLES = [ "Officer", "Admin" ].freeze
 
   has_secure_password
-
-  validates :user_name, presence: true, format: { without: /\s/, message: "must contain no spaces" }
-  validates :email_address, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "is invalid" }
+  validates :phone_number, phone_number: true, allow_blank: true
   validates :role, inclusion: { in: VALID_ROLES }
+  with_options uniqueness: { case_sensitive: false } do
+    validates :user_name, presence: true, format: { without: /\s/, message: "must contain no spaces" }
+    validates :email_address, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "is invalid" }
+  end
 
   with_options if: :is_admin? do |admin|
     admin.validates :first_name, :last_name, :phone_number, presence: true, on: :update
