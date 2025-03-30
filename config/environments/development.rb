776f863925cf57f3e842ce84bfa80fc0c91aa102
Bright Issah <bphiri.aki@gmail.com>
2025-03-30 16:39:41 +0200

    Update jon queue adapter

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  config.active_job.queue_adapter = :async
+  config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Don't care if the mailer can't send.
