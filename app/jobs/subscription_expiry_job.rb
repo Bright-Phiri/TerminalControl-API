1ffc72bd3b8706c70b20780a20234de8794e9abd
Bright Phiri <bphiri.aki@gmail.com>
2025-05-04 05:17:22 +0200

    WIP

--- app/jobs/subscription_expiry_job.rb
+++ app/jobs/subscription_expiry_job.rb
@@ -2,9 +2,15 @@
 
 class SubscriptionExpiryJob < ApplicationJob
   queue_as :default
+    EXPIRY_NOTICE_WINDOW = 10.days
     def perform
-      # Find subscriptions that will expire in the next 7 days
-      subscriptions = Subscription.where("end_date BETWEEN ? AND ?", Time.current, Time.current + 7.days)
+      # Find subscriptions that will expire in the next 10 days
+      subscriptions = Subscription.where("end_date BETWEEN ? AND ?", Time.current, Time.current +  EXPIRY_NOTICE_WINDOW).where.not(status: Subscription.statuses[:expired])
+      if subscriptions.blank?
+        Rails.logger.info "[SubscriptionExpiryJob] No subscriptions expiring within #{EXPIRY_NOTICE_WINDOW.inspect}."
+        return
+      end
+
       subscriptions.find_each do |subscription|
         SubscriptionMailer.with(subscription: subscription).subscription_about_to_expire.deliver_later
       end
