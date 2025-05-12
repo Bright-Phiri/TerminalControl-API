d05b21859d482d1bdab9a4b05f3bf51f66b63418
Bright Issah <bphiri.aki@gmail.com>
2025-05-12 10:44:37 +0200

    Update subscription reminder message

--- app/jobs/subscription_expiry_job.rb
+++ app/jobs/subscription_expiry_job.rb
@@ -2,17 +2,24 @@
 
 class SubscriptionExpiryJob < ApplicationJob
   queue_as :default
-    EXPIRY_NOTICE_WINDOW = 10.days
-    def perform
-      # Find subscriptions that will expire in the next 10 days
-      subscriptions = Subscription.where("end_date BETWEEN ? AND ?", Time.current, Time.current +  EXPIRY_NOTICE_WINDOW).where.not(status: Subscription.statuses[:expired])
-      if subscriptions.blank?
-        Rails.logger.info "[SubscriptionExpiryJob] No subscriptions expiring within #{EXPIRY_NOTICE_WINDOW.inspect}."
-        return
-      end
 
-      subscriptions.find_each do |subscription|
-        SubscriptionMailer.with(subscription: subscription).subscription_about_to_expire.deliver_later
-      end
+  EXPIRY_NOTICE_WINDOW = 10.days
+
+  def perform
+    start_date = Date.current
+    end_date = start_date + EXPIRY_NOTICE_WINDOW
+
+    subscriptions = Subscription.where("end_date BETWEEN ? AND ?", start_date, end_date)
+                                .where.not(status: Subscription.statuses[:expired])
+
+    if subscriptions.blank?
+      Rails.logger.info "[SubscriptionExpiryJob] No subscriptions expiring between #{start_date} and #{end_date}."
+      return
     end
+
+    subscriptions.find_each do |subscription|
+      Rails.logger.info "[SubscriptionExpiryJob] Sending expiry notice for subscription ID #{subscription.id}, ends on #{subscription.end_date}."
+      SubscriptionMailer.with(subscription: subscription).subscription_about_to_expire.deliver_later
+    end
+  end
 end
--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -4,6 +4,7 @@ class SubscriptionMailer < ApplicationMailer
   def subscription_about_to_expire
     @subscription = params[:subscription]
     @taxpayer = @subscription.taxpayer
+    @days_left = (@subscription.end_date - Date.current).to_i
 
     mail(to: @taxpayer.email_address, subject: "Reminder: Your subscription is expiring soon")
   end
