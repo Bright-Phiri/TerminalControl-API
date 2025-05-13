ba58eacd2d9dbc305bacffa2a9e15de9acddfa82
Bright Issah <bphiri.aki@gmail.com>
2025-05-13 10:28:13 +0200

    Fix subscription reminder to avoid displaying "0 days" on expiry date

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -4,7 +4,9 @@ class SubscriptionMailer < ApplicationMailer
   def subscription_about_to_expire
     @subscription = params[:subscription]
     @taxpayer = @subscription.taxpayer
+
     @days_left = (@subscription.end_date - Date.current).to_i
+    @days_left = 1 if @days_left == 0
 
     mail(to: @taxpayer.email_address, subject: "Reminder: Your subscription is expiring soon")
   end
