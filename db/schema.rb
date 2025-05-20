829e1dea12d517604fdc4f99237cecfb958079f0
Bright Phiri <bphiri.aki@gmail.com>
2025-05-20 18:23:16 +0200

    Update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_02_091509) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_20_162120) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_02_091509) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "expiry_notice_sent", default: false
     t.index ["taxpayer_id"], name: "index_subscriptions_on_taxpayer_id"
   end
 
