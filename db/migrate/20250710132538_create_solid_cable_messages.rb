79883e14f632693db45f460a6f4715ce0657bffc
Bright Phiri <bphiri.aki@gmail.com>
2025-07-10 15:40:10 +0200

    Configure single database for Solid Cable and queue

--- db/migrate/20250710132538_create_solid_cable_messages.rb
+++ db/migrate/20250710132538_create_solid_cable_messages.rb
@@ -1,14 +1,13 @@
 class CreateSolidCableMessages < ActiveRecord::Migration[8.0]
   def change
-    create_table "solid_cache_entries", force: :cascade do |t|
-      t.binary "key", limit: 1024, null: false
-      t.binary "value", limit: 536870912, null: false
+    create_table "solid_cable_messages", force: :cascade do |t|
+      t.binary "channel", null: false
+      t.binary "payload", null: false
       t.datetime "created_at", null: false
-      t.integer "key_hash", limit: 8, null: false
-      t.integer "byte_size", limit: 4, null: false
-      t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
-      t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
-      t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
-    end
+      t.bigint "channel_hash", null: false
+      t.index ["channel"], name: "index_solid_cable_messages_on_channel"
+      t.index ["channel_hash"], name: "index_solid_cable_messages_on_channel_hash"
+      t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
+  end
   end
 end
