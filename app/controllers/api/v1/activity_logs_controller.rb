0c2579c898c23fbfe16c181b28cd28d38587e688
Bright Issah <bphiri.aki@gmail.com>
2025-07-16 16:30:20 +0200

    WIP

--- app/controllers/api/v1/activity_logs_controller.rb
+++ app/controllers/api/v1/activity_logs_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::ActivityLogsController < ApplicationController
+
   def index
     logs = params[:search].present? ? Log.search(params[:search]) : Log.all
     logs = logs.paginate(page: params[:page] || 1, per_page: params[:per_page] || 10)
--- test/controllers/internal/ping_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Internal::PingControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
